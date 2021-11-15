9b08256f4c2c620205a9fd97b1cad1a1df4c1885
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2021-11-15 16:50:47 +0100

    Create main.c

---
+++ lab07/genCode/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variabiliglobali.h"
+#include "define.h"
+
+void main() {
+	printf("NUM: %d\n", NUM);
+	printf("Conta: %d, var1: %d\n", conta(), var1);
+}
