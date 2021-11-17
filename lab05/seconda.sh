788fa7116157361279c44ccbf32ce6f6ac7165e4
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2021-11-17 18:26:09 +0100

    Move backuped file back into lab05 dir

--- lab05/seconda.sh
+++ lab05/seconda.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if (( $# == 1 )); then 
-	OUT=""
-	while read prima seconda altro; do
-		if [[ -n $seconda ]]; then
-			OUT="${OUT}${seconda}"
-		fi
-	done < $1
-	echo $OUT
-fi
+# nome_file = $1
+
+if [[ $# != 1 ]]; then echo "Devi passare 1 file"; exit 1; fi
+if [[ ! -r $1 ]]; then echo "File inesistente o illeggibile"; exit 2; fi
+
+OUT=""
+
+while read prima seconda altro; do
+	OUT="${OUT}${seconda}"
+done < $1
+
+echo $OUT
