39762d4670a90a4290538a8082167dbfab87e357
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2021-12-04 16:42:11 +0100

    Complete es insuff2

--- lab06/insuff2.sh
+++ lab06/insuff2.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-grepStr=""
-while read surname name matr mark; do
-	if [[ $mark -lt 18 ]]; then
-		grepStr="${grepStr} ${matr}"
+while read surname2 name2 matr2 mark2; do
+	if [[ $mark2 -lt 18 ]]; then
+		foundFlag=0
+		while read sur name matr mark; do
+			if [[ $matr2 -eq $matr ]]; then
+				(( foundFlag = 1 ))
+			fi
+		done < RisultatiProvaPratica1.txt
+
+		if (( $foundFlag == 0 )); then
+			echo "${matr2} ${surname2} ${name2} ${mark2}"
+		fi
 	fi
 done < RisultatiProvaPratica2.txt
-
-for matr2 in ${grepStr}; do
-	foundFlag=0
-	res=""
-	while read sur name matr mark; do
-		res="$matr $sur $name $mark"
-		if [[ $matr2 -eq $matr ]]; then
-			foundFlag=1
-		fi
-	done < RisultatiProvaPratica1.txt
-
-	if (( foundFlag == 0 )); then echo $res; fi
-done
