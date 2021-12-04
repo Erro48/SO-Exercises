9ba2fba379a477176f2fc98ac17b9a5b08c39019
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2021-12-04 16:43:08 +0100

    Refactor struttura.c using funcitons

--- lab08/strutture/struttura.c
+++ lab08/strutture/struttura.c
@@ -13,34 +13,73 @@ typedef struct struttura {
 	int Indice;
 } struttura;
 
+void setStr(struttura* s);
+void printStruct(struttura s);
+void printStructShort(struttura s);
+void createThreadCtrlError(pthread_t* tid, void* args);
+void* threadBody(void* args);
+
+void setStr(struttura* s) {
+	char* sindice[2];
+	snprintf(s->Str, STRLEN, "%d", s->N);
+	snprintf(sindice, 2, "%d", s->Indice);
+	strcat(s->Str, " ");
+	strcat(s->Str, sindice);
+}
+
+void printStruct(struttura s) {
+	printf("Struttura:\n");
+	printf(" - N:\t\t%i\n", s.N);
+	printf(" - Str:\t\t%s\n", s.Str);
+	printf(" - Indice:\t%i\n\n", s.Indice);
+}
+
+void printStructShort(struttura s) {
+	int i,j;
+	for (i=0; i<NTHREAD - 1 - s.N; i++) {
+		printf("\t");
+	}
+	for (j=0; j<s.Indice; j++) {
+		printf(" ");
+	}
+	printf("{ %s }\n", s.Str);
+}
+
+void createThreadCtrlError(pthread_t* tid, void* args) {
+	int err;
+	err = pthread_create(tid, NULL, threadBody, args);
+	if (err != 0) {
+		printf("[ERROR] %d\n", err);
+	}
+}
+
 void * threadBody(void* arg) {
+	/* Aspetta 1 secondo */
 	sleep(DELAY);
+	/* Legge il contenuto della struttura ricevuta */
 	struttura *arr = (struttura*)arg;
-	printf("[DEBUG] N: %i, Indice: %i\n", arr->N, arr->Indice);
-	char msg[STRLEN], nstr[2], indicestr[2];
-	snprintf(nstr, 2, "%d", arr->N);
-	snprintf(indicestr, 2, "%d", arr->Indice);
-	strcpy(msg, nstr);
-	strcat(msg, " ");
-	strcat(msg, indicestr);
-	strcpy(arr->Str, msg);
+	setStr(arr);
+	printStructShort(*arr);
+
 	if (arr->N > 1) {
 		int i;
-		int *val;
 		pthread_t* tid;
 		struttura *args;
 		tid = (pthread_t*)malloc(sizeof(pthread_t) * arr->N);
-		args = (struttura*)malloc(sizeof(struttura) * arr->N);
-		for (i=0; i<arr->N; i++) {
-			val = (int*)malloc(sizeof(int));
-			(args + i)->N = arr->N - 1;
-			(args + i)->Indice = i;
+		for (i = 0; i < arr->N - 1; i++) {
+			args = (struttura*)malloc(sizeof(struttura));
+			args->N = arr->N - 1;
+			args->Indice = i;
+/*			printf("\n------------| %i |------------\n%i %i\n", i, (args + i)->N, (args + i)->Indice);*/
 			pthread_create((tid + i), NULL, threadBody, (void*)args);
 		}
-	} else {
-		printf("Fine thread [%s]\n", arr->Str);
+		for (i = 0; i < arr->N - 1; i++) {
+			pthread_join((tid + i), (void**)&args);
+			printf("Dopo join ");
+			printStructShort(*args);
+		}
 	}
-	pthread_exit(NULL);
+	pthread_exit(arr);
 	return NULL;
 }
 
@@ -49,12 +88,16 @@ int main() {
 	int errCreate;
 	struttura arr[NTHREAD];
 	pthread_t tid[NTHREAD];
+	struttura *str;
 	for (i = 0; i < NTHREAD; i++) {
 		arr[i].N = NTHREAD - 1;
 		arr[i].Indice = i;
 		printf("Creato il thread %i\n", i);
 		errCreate = pthread_create(&tid[i], NULL, threadBody, (void*)&arr[i]);
 		if(errCreate != 0) printf("Errore: %i\n", errCreate);
+	}
+	for (i = 0; i < NTHREAD; i++) {
+		pthread_join(&tid[i], (void**)&str);
 	}
 	pthread_exit(0);
 	return 0;
