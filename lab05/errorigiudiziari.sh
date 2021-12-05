b5f80bafbc9bf792b02734f27b68ef716374c393
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2021-12-05 16:37:48 +0100

    Complete errorigiudiziari.sh

--- lab05/errorigiudiziari.sh
+++ lab05/errorigiudiziari.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+while read idVerdetto msgVerdetto; do
+    idPers=`grep $idVerdetto processi.txt | cut -b 1-2`
+    grep $idPers denunce.txt | \
+        while read nome cognome id reato; do
+            echo "${nome} ${cognome}:"
+            echo "Reato: ${reato}."
+            echo "Verdetto: ${msgVerdetto}."
+            echo ""
+        done
+done < verdetti.txt
